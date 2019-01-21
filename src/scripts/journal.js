a8d81546c32a0d79541cd76e617fccb5ca51ef92
James Farrell <jamesfarrellmusic@gmail.com>
2019-01-21 15:59:48 -0600

    simplified adding component to DOM

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,47 +1,6 @@
 const button = document.getElementById("submitButton")
 let journalArray = []
 
-const makeJournalEntryComponent = (journalEntries) => {
-    // main div
-    const entry = document.createElement("section")
-    entry.className = "printedJournalEntry"
-    
-    // header div
-    const headerDiv = document.createElement("div")
-    headerDiv.className = "journalHeaderDiv"
-    entry.appendChild(headerDiv)
-    
-    // create date element
-    const entryDate = document.createElement("div")
-    entryDate.className = "printedDate print"
-    entryDate.textContent = `${journalEntries.date}`
-    headerDiv.appendChild(entryDate)
-    
-    // create concepts element
-    const entryConcepts = document.createElement("div")
-    entryConcepts.className = "printedConcepts print"
-    entryConcepts.textContent = `${journalEntries.concepts}`
-    headerDiv.appendChild(entryConcepts)
-    
-    //create mood element
-    const entryMood = document.createElement("div")
-    entryMood.className = "printedMood print"
-    entryMood.textContent = `${journalEntries.mood}`
-    headerDiv.appendChild(entryMood)
-    
-    // append to headerDiv
-    // document.querySelector(".entryLog").appendChild(headerDiv)
-    
-    // create journal entry element
-    const entryEntry = document.createElement("div")
-    entryEntry.className = "printedEntry print"
-    entryEntry.textContent = `${journalEntries.entry}`
-    entry.appendChild(entryEntry)
-    
-    // append to DOM
-    document.querySelector(".entryLog").appendChild(entry)
-}
-
 button.onclick = (e) => {
     e.preventDefault()
 
@@ -61,8 +20,22 @@ button.onclick = (e) => {
     }
 
     journalArray.push(journalEntries)
-    makeJournalEntryComponent(journalEntries)
+    let domEntry = makeJournalEntryComponent(journalEntries)
+    document.querySelector(".entryLog").innerHTML += domEntry
     document.getElementById("myForm").reset();
     console.log(journalArray)
 
+}
+const makeJournalEntryComponent = (journalEntries) => {
+    
+   return `
+   <section class="printedJournalEntry">
+    <div class="journalHeaderDiv">
+        <section class="printedDate print">${journalEntries.date}</section>
+        <section class="printedConcepts print">${journalEntries.concepts}</section>
+        <section class="printedMood print">${journalEntries.mood}</section>
+        </div>
+        <section class="printedEntry print">${journalEntries.entry}</section>
+        </section>
+        `
 }

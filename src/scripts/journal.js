1836050a0012c3893323ff75c20ef55f71974e37
James Farrell <jamesfarrellmusic@gmail.com>
2019-01-22 15:11:28 -0600

    modularized code

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,48 +1,13 @@
-const button = document.getElementById("submitButton")
-let journalArray = []
+/*
+    Main application logic that uses the functions and objects
+    defined in the other JavaScript files.
 
-button.onclick = (e) => {
-    e.preventDefault()
+    Change the fake variable names below to what they should be
+    to get the data and display it.
+*/
 
-        
-
-    let date = document.getElementById("journalDate").value
-    let concepts = document.getElementById("conceptsInput").value
-    let entry = document.getElementById("journalEntry").value
-    let mood = document.getElementById("moodBox").value
-
-    let journalEntries =
-    {
-        date: date,
-        concepts: concepts,
-        entry: entry,
-        mood: mood
-    }
-    journalArray.push(journalEntries)  
-}
-const makeJournalEntryComponent = (entries) => {
-    
-    return `
-    <section class="printedJournalEntry">
-    <div class="journalHeaderDiv">
-    <section class="printedDate print">${entries.date}</section>
-    <section class="printedConcepts print">${entries.concepts}</section>
-    <section class="printedMood print">${entries.mood}</section>
-    </div>
-    <section class="printedEntry print">${entries.entry}</section>
-    </section>
-    `
-}
-
-fetch("http://localhost:3000/entries")
-.then(entries => entries.json())  // Parse as JSON
-.then(parsedEntries => {
-    console.table(parsedEntries)
-    parsedEntries.forEach(entry => {
-        let domEntry = makeJournalEntryComponent(entry)
-        document.querySelector(".entryLog").innerHTML += domEntry
-        document.getElementById("myForm").reset();
-        console.log(journalArray)
+API.getJournalEntries().then(parsedEntries => {
+    domFunction(parsedEntries)
     })
-    
-    })
+
+
